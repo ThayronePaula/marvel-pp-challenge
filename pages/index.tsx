8722e5286bb87077cfb5fde8cc7d01cccc3ad8e2
thayrone <thayronedepaula15@gmail.com>
2021-10-27 21:18:45 -0300

    feat: add material-ui

--- pages/index.tsx
+++
@@ -1,3 +0,0 @@
-const Index = () => <h1>Next with TypeScript</h1>;
-
-export default Index;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import { Typography } from "@mui/material";
+const Index = () => <Typography variant='h1' >Next with TypeScript</Typography>;
+
+export default Index;
